fa692d11b3b0243d0dff2b060b65aed0516b5804
test <test@MacBook-Pro-de-test.local>
2023-03-15 17:14:30 -0500

    feat: inicial set up server

---
+++ models/server.js
@@ -0,0 +1,23 @@
+const express = require('express')
+
+class Server {
+    constructor(){
+        this.app = express()
+        this.port = process.env.PORT
+        this.routes()
+    }
+
+    routes() {
+        this.app.get('/', function (req, res) 
+        {
+            res.send('Hello World')
+        }
+        )
+    }
+
+    listen(){
+        this.app.listen(this.port)
+    }
+}
+
+module.exports = Server
